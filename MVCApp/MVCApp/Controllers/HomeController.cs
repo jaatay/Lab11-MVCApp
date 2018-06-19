872d24d557463c075ea50e53261103870b1ef213
Jesse Atay <jesse.atay@gmail.com>
2018-06-19 14:42:58 -0700

    updates

--- MVCApp/MVCApp/Controllers/HomeController.cs
+++ MVCApp/MVCApp/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 
 namespace MVCApp
 {
 	public class HomeController: Controller
 	{
-		public Class1()
-		{
-		}
+		
 	}
 }
